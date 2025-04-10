f8a97cfecade18165c064d25439c9e5ee1fa70e5
SKR4VV <>
2025-04-10 15:49:15 +0200

    build: initialize java project & dependencies

---
+++ src/main/java/com/chronophage/ChronophageApplication.java
@@ -0,0 +1,13 @@
+package com.chronophage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronophageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronophageApplication.class, args);
+	}
+
+}
