6f4ab2fa6b8049848d3f60fa214d2d1693147674
SKR4VV <>
2025-04-10 17:30:40 +0200

    feat: stock components

---
+++ src/main/java/com/chronophage/stock/ProductStockRepository.java
@@ -0,0 +1,9 @@
+package com.chronophage.stock;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductStockRepository extends JpaRepository<ProductStockEntity, Long> {
+
+}
