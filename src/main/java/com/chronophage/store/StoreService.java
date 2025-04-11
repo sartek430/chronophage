b26956cc1d26f33692e427d168b6a60b1817c48c
SKR4VV <>
2025-04-11 14:15:22 +0000

    feat: register store on init

--- src/main/java/com/chronophage/store/StoreService.java
+++ src/main/java/com/chronophage/store/StoreService.java
@@ -1,12 +1,24 @@
 package com.chronophage.store;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class StoreService {
     @Autowired RestClient restClient;
+
+    @Value("${remote.store.name}") String name;
+    @Value("${remote.store.baseurl}") String baseUrl;
+
+    @PostConstruct
+    public void init() {
+        StoreDTO store = new StoreDTO(name, baseUrl);
+        registerStore(store);
+    }
 
     public StoreDTO registerStore(StoreDTO store) {
         return restClient.post()
