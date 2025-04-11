1ae8d005cae5f930480d674b8e6268130c8d6cbb
SKR4VV <>
2025-04-11 09:42:57 +0000

    test: should get all products

---
+++ src/test/java/com/chronophage/ProductServiceTest.java
@@ -0,0 +1,47 @@
+package com.chronophage;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.chronophage.product.ProductEntity;
+import com.chronophage.product.ProductRepository;
+import com.chronophage.product.ProductService;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+    
+    @Mock ProductRepository repository;
+    @InjectMocks ProductService service;
+
+    private List<ProductEntity> products;
+
+    @BeforeEach
+    public void setup() {
+        products = Arrays.asList(
+            new ProductEntity("potatoes", 1000L, 0.35),
+            new ProductEntity("tomatoes", 500L, 0.75)
+        );
+    }
+
+    @Test
+    public void testGetAll() {
+        when(repository.findAll()).thenReturn(products);
+        List<ProductEntity> result = service.getAll();
+
+        assertEquals(2, result.size());
+        assertEquals("potatoes", result.get(0).getName());
+        
+        verify(repository, times(1)).findAll();
+    }
+}
